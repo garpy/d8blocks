807773ca37e8b3f5478cc5edcf70b0e3aed8959a
Gary Pryde <gary@garypryde.com>
2016-01-17 16:08:15 -0800

    added step three to HelloBlock.php folder

---
+++ modules/custom/hello_world/src/Plugin/Block/HelloBlock.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Provides a 'Hello' Block
+ *
+ * @Block(
+ *   id = "hello_block",
+ *   admin_label = @Translation("Hello block"),
+ * )
+ */
+
+namespace Drupal\hello_world\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+class HelloBlock extends BlockBase implements BlockPluginInterface {
+    /**
+     * {@inheritdoc}
+     */
+    public function build() {
+        return array(
+            '#markup' => $this->t('Hello, World!'),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function blockForm($form, FormStateInterface $form_state) {
+        $form = parent::blockForm($form, $form_state);
+
+        $config = $this->getConfiguration();
+
+        $form['hello_block_name'] = array (
+            '#type' => 'textfield',
+            '#title' => $this->t('Who'),
+            '#description' => $this->t('Who do you want to say hello to?'),
+            '#default_value' => isset($config['name']) ? $config['name'] : ''
+        );
+
+        return $form;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function blockSubmit($form, FormStateInterface $form_state) {
+        $this->setConfigurationValue('name', $form_state->getValue('hello_block_name'));
+    }
+}
+?>
+
+
